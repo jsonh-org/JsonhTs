e652100d42db5dcd7148e4f4dff933b8f6a1bf44
Joyless <65855333+Joy-less@users.noreply.github.com>
2026-02-19 22:40:33 +0000

    Bump version

--- jsonh-ts/build/result-helpers.d.ts
+++
@@ -1,6 +0,0 @@
-declare function valueOrThrow<T>(value: T | Error): T;
-declare const _default: {
-    valueOrThrow: typeof valueOrThrow;
-};
-export = _default;
-//# sourceMappingURL=result-helpers.d.ts.map
